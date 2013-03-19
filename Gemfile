f1caa2322a6a2b3338c8f9da65f5ae09e97afe88
meriororen <meriororen@gmail.com>
2013-03-20 06:59:40 +0900

    BDD preparation: Ran the cucumber and rspec generators

--- Gemfile
+++ Gemfile
@@ -7,6 +7,13 @@ gem 'rails', '3.2.12'
 
 group :development, :test do
   gem 'sqlite3'
+  gem 'rspec-rails', '~> 2.5'
+end
+
+group :test do
+  gem 'cucumber-rails'
+  gem 'capybara'
+  gem 'database_cleaner'
 end
 
 gem 'devise', '~> 1.4.3'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130319084730) do
+ActiveRecord::Schema.define(:version => 20130319215158) do
 
   create_table "books", :force => true do |t|
     t.string   "title"
@@ -33,5 +33,23 @@ ActiveRecord::Schema.define(:version => 20130319084730) do
     t.datetime "updated_at",                :null => false
     t.integer  "chapter_id"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                         :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                                            :null => false
+    t.datetime "updated_at",                                            :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
